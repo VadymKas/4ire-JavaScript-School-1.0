7f5b39f7e518cd6a04a69967b35226280cf211f6
Vadym Kaschin <62203607+VadymKas@users.noreply.github.com>
2021-11-09 16:46:25 +0200

    Final HW3.js
    Added example 2

--- HW3.js
+++ HW3.js
@@ -1,2 +1,15 @@
+//example 1
+
 let filterBy = (ar, selector) => ar.filter( elem => typeof(elem) != selector );
+filterBy(['hello', 'world', 23, '23', null], 'string'); // [23, null]
+
+
+//example 2
+
+let filterBy = (arr, selector) => {
+  let newArray = [];  
+  arr.map(item => typeof(item) != selector ? newArray.push(item) : 'not selected' );
+  return newArray;
+}
+
 filterBy(['hello', 'world', 23, '23', null], 'string'); // [23, null]
