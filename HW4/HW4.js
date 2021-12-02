1eea643c19e820f3b62e52b580e76ebfc5b7efa7
Vadym Kaschin <62203607+VadymKas@users.noreply.github.com>
2021-12-02 10:47:02 +0200

    Final HW4
    All done.

--- HW4/HW4.js
+++ HW4/HW4.js
@@ -15,26 +15,41 @@ function randomCity() {
 
   let randomazer = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
 
-  let createList = (type, outputDay, outputNight, inputDay, inputNight, flats, price, capacity, quantity) => Array.from({ length: randomazer(1, quantity) }, 
-    () => new Construction(type, randomazer(1, outputDay), randomazer(1, outputNight), inputDay, inputNight, randomazer(1, flats), randomazer(2, price), randomazer(95, capacity)));
-  
+  let createList = (type, outputDay, outputNight, inputDay, inputNight, flats, price, capacityMin, capacityMax, quantityMin, quantityMax) => Array.from({ length: randomazer(quantityMin, quantityMax) }, () => new Construction(type, randomazer(1, outputDay), randomazer(1, outputNight), inputDay, inputNight, randomazer(1, flats), randomazer(1, price), randomazer(capacityMin, capacityMax)));
+
   let electricityСounting = (el, day, night, flats) => el.reduce((sum, cur) => sum + (cur[day] * 16 + cur[night] * 8) * cur[flats], 0).toFixed(2);
 
-  let coalPlant = createList('Coal plant', 100, 100, 0, 0, 1, 0, 0, 3),
-      solarPlant = createList('Solar plant', 5, 0, 0, 0, 1, 0, 0, 20),
-      cityHouses = createList('House', 0, 0, 0.004, 0.001, 400, 0, 0, 300),
-      powerLines = createList('Power lines', -1, -1, 0, 0, 1, 3, 105, 4);
+  let coalPlant = createList('Coal plant', 100, 100, 0, 0, 1, -1, 0, 0, 1, 3),
+    solarPlant = createList('Solar plant', 5, -1, 0, 0, 1, -1, 0, 0, 1, 20),
+    cityHouses = createList('House', -1, -1, 0.004, 0.001, 400, 0, 0, 0, 1, 300),
+    powerLines = createList('Power lines', -1, -1, 0, 0, 1, 3, 60, 100, 3, 5);
 
   let coalPlantProduction = +electricityСounting(coalPlant, 'outputDay', 'outputNight', 'flats'),
-      solarPlantProduction = +electricityСounting(solarPlant, 'outputDay', 'outputNight', 'flats'),
-      cityHousesComsumption = +electricityСounting(cityHouses, 'inputDay', 'inputNight', 'flats'),
-      powerLinesCapacity = +electricityСounting(powerLines, 'capacity', 'capacity', 'flats');
+    solarPlantProduction = +electricityСounting(solarPlant, 'outputDay', 'outputNight', 'flats'),
+    cityHousesComsumption = +electricityСounting(cityHouses, 'inputDay', 'inputNight', 'flats'),
+    powerLinesCapacity = +electricityСounting(powerLines, 'capacity', 'capacity', 'flats');
 
-  let temp = (coalPlantProduction + solarPlantProduction - cityHousesComsumption).toFixed(2);
+  let difference = +(coalPlantProduction + solarPlantProduction - cityHousesComsumption).toFixed(2),
+      temp = Math.abs(difference),
+      money = 0;
 
-  console.log(`Coal plants electricity production per day - ${coalPlantProduction} mW`);
-  console.log(`Solar plants electricity production per day - ${solarPlantProduction} mW`);
-  console.log(`City's electricity usage per day - ${cityHousesComsumption} mW`);
-  console.log(`Power lines capacity - ${powerLinesCapacity} mW`); 
-  console.log(temp);
-}
+  function budget(powerLines) {
+    for (let i = 0; i < powerLines.length; i++) {
+      if (temp > powerLines[i].capacity * 24) {
+        money += powerLines[i].capacity * 24 * powerLines[i].price;
+        temp -= powerLines[i].capacity * 24;
+      } else {
+        money += +(temp * powerLines[i].price).toFixed(2);
+        break;
+      }
+    }
+    (difference > 0) ? console.log(`You can sell unneeded electricity for ${money} $`) 
+                     : console.log(`You have to buy the missing electricity for ${money} $`);
+  }
+    console.log(`Coal plants electricity production per day - ${coalPlantProduction} mW`);
+    console.log(`Solar plants electricity production per day - ${solarPlantProduction} mW`);
+    console.log(`City's electricity usage per day - ${cityHousesComsumption} mW`);
+    console.log(`Power lines capacity - ${powerLinesCapacity} mW`);
+    console.log(budget(powerLines));
+  }
+  randomCity();
